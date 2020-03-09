93ba34d3c24632617d2e1558a20bb54156a3b418
Davina Tan <davina.tan@connect.qut.edu.au>
2020-03-09 12:18:17 +1000

    uploaded teppanyaki site

---
+++ teppanyaki_site/teppanyaki.js
@@ -0,0 +1,94 @@
+// Automatically Hide Navbar in Dropdown
+
+$('.navbar-nav li a').on('click', function() {
+	if (!$(this).hasClass('dropdown-toggle')) {
+		$('.navbar-collapse').collapse('hide')
+	}
+})
+
+// Active Navbar Effect
+
+$(document).ready(function() {
+	$('.nav a').on('click', function() {
+		$('.nav').find('.active').removeClass('active')
+		$(this).addClass('active')
+	})
+})
+
+// Active Menu Effect
+
+$('.btn-link').click(function() {
+	$('.btn-link.colour').removeClass('colour')
+	$(this).toggleClass('colour')
+})
+
+// Navbar Scroll Effect
+
+// (function ($) {
+//     $(document).ready(function(){
+//       $(".navbar").hide();
+
+//       $(function () {
+//           $(window).scroll(function () {
+//               if ($(this).scrollTop()) {
+//                   $('.navbar').stop().fadeIn(500);
+//               } else {
+//                   $('.navbar').stop().fadeOut(500);
+//               }
+//           });
+//       });
+//   });
+// }(jQuery));
+
+// Contact Link Hover Effects
+
+$('.facebook-link')
+	.mouseenter(function() {
+		$('.facebook-link').addClass('colour')
+	})
+	.mouseleave(function() {
+		$('.facebook-link').removeClass('colour')
+	})
+
+$('.address-link')
+	.mouseenter(function() {
+		$('.address-link').addClass('colour')
+	})
+	.mouseleave(function() {
+		$('.address-link').removeClass('colour')
+	})
+
+// Scroll Animation
+
+$(document).ready(function() {
+	$('section').css('width', $(window).width())
+	$(window).resize(function() {
+		$('section').css('width', $(window).width())
+	})
+})
+
+var slideLeft = {
+	duration: 1000,
+	distance: '300px',
+	origin: 'left',
+	viewFactor: 0.4
+}
+
+var slideRight = {
+	duration: 1000,
+	distance: '300px',
+	origin: 'right',
+	viewFactor: 0.4
+}
+
+ScrollReveal().reveal('.history-box', slideLeft)
+
+ScrollReveal().reveal('.history-image', slideRight)
+
+ScrollReveal().reveal('.gallery-image1', slideLeft)
+
+ScrollReveal().reveal('.gallery-image2', slideRight)
+
+ScrollReveal().reveal('.gallery-image3', slideLeft)
+
+ScrollReveal().reveal('.gallery-image4', slideRight)
